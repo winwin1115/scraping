13e3dc38845ef37378b2d03111c10cf659b2a3a7
Erik Peterson <ericpeterson.lancer@gmail.com>
2021-10-15 15:05:34 +0900

    header update
    header update

--- resources/views/admin/layouts/partials/header.blade.php
+++ resources/views/admin/layouts/partials/header.blade.php
@@ -12,10 +12,6 @@
     <ul class="action-list">
       	<li>
 	        <a href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="avatar"><img src="{{asset('/assets/admin/img/avatars/avatar1.png')}}" alt="Avatar"></a>
-        	<div class="dropdown-menu dropdown-menu-right notification-dropdown">
-          		<a class="dropdown-item" href="/admin/settings/social"><i class="icon-fa icon-fa-cogs"></i> Settings</a>
-          		<a class="dropdown-item" href="/logout"><i class="icon-fa icon-fa-sign-out"></i> Logout</a>
-        	</div>
       	</li>
     </ul>
 </header>
