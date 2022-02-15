179f879f29aaf8c9d195b3e5305e70abdb45933f
wenjie.yin970 <86898936+Erik1115@users.noreply.github.com>
2022-02-15 17:41:08 +0900

    Update AsinController.php
    add price2

--- app/Http/Controllers/AsinController.php
+++ app/Http/Controllers/AsinController.php
@@ -364,10 +364,11 @@ class AsinController extends Controller
 
         $price = '';
         $price_temp = [];
-        $price_temp = $pokemon_xpath->query('//div[@id="corePrice_feature_div"]//div[@class="a-section a-spacing-micro"]//span[@class="a-offscreen"]/text()');
-        if(count($price_temp))
+        $price1_temp = $pokemon_xpath->query('//div[@id="corePrice_feature_div"]//div[@class="a-section a-spacing-micro"]//span[@class="a-offscreen"]/text()');
+        $price2_temp = $pokemon_xpath->query('//div[@class="twisterSlotDiv "]//span[@class="size_name_0_price"]//span[@class="a-size-mini olpWrapper"]/text()');
+        if(count($price1_temp))
         {
-            foreach($price_temp as $item)
+            foreach($price1_temp as $item)
                 $price_value = $item->nodeValue;
             if($price_value)
             {
@@ -376,6 +377,19 @@ class AsinController extends Controller
                 $data['variant_price'] = (float)$price * $currency_rate * $profit_rate;
                 $data['variant_compare_price'] = (float)$price * $currency_rate * 1.1;
             }
+        }
+        else if(count($price2_temp))
+        {
+            foreach($price2_temp as $item)
+                $price_value = $item->nodeValue;
+            if($price_value)
+            {
+                $price = explode("¥", $price_value)[1];
+                $price = trim(str_replace(',', '', $price));
+                $data['variant_price'] = (float)$price * $currency_rate * $profit_rate;
+                $data['variant_compare_price'] = (float)$price * $currency_rate * 1.1;
+            }
+            dd($price);
         }
         else
         {
